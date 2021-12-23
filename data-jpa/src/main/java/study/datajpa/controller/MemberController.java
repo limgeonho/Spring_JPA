cf0f5f1212d14833b51cb45de3e10e0bb277f8ab
limgeonho <ghlim909@gmail.com>
2021-12-24 03:10:17 +0900

    jpa 12.24 finish

--- data-jpa/src/main/java/study/datajpa/controller/MemberController.java
+++ data-jpa/src/main/java/study/datajpa/controller/MemberController.java
@@ -1,9 +1,14 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -27,8 +32,23 @@ public class MemberController {
         return member.getUsername();
     }
 
-    @PostConstruct
+    @PostConstruct // spring이 실행되면서 먼저 실행
     public void init(){
         memberRepository.save(new Member("userA"));
     }
+
+    @GetMapping("/members")
+    public Page<MemberDto> list(Pageable pageable){
+//        직접 페이징 시작 숫자를 1로 설정하는 방법!!!
+//        PageRequest request = PageRequest.of(1,2);
+//        Page<Member> page = memberRepository.findAll(request);
+        Page<Member> page = memberRepository.findAll(pageable);
+        Page<MemberDto> map = page.map(member -> new MemberDto(member.getId(), member.getUsername(), null));
+        return map;
+    }
+//    @GetMapping("/members")
+//    public Page<Member> list(@PageableDefault(size = 5) Pageable pageable){
+//        Page<Member> page = memberRepository.findAll(pageable);
+//        return page;
+//    }
 }
